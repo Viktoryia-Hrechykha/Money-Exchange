fa909b786e7ac8d3c3c1dac6109b94c0ec52649f
Viktoryia_Hrechykha <viktoryia_hrechykha@epam.com>
2018-09-22 12:57:11 +0300

    answer_money_exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let answer = {};
+
+    if (currency <= 0) {
+        return answer;
+    }
+
+    if(currency > 10000) {
+        answer.error = "You are rich, my friend! We don't have so much coins for exchange";
+        return answer;
+    }
+
+    let h = Math.floor(currency / 50);
+    let hRest = (currency % 50);
+
+    let q = Math.floor(hRest / 25);
+    let qRest = (hRest % 25);
+
+    let d = Math.floor(qRest / 10);
+    let dRest = (qRest % 10);
+
+    let n = Math.floor(dRest / 5);
+    let nRest = (dRest % 5);
+
+    let p = currency - 50*h - 25*q - 10*d - 5*n;
+
+    if (h) {answer["H"] = h}
+    if (q) {answer["Q"] = q}
+    if (d) {answer["D"] = d}
+    if (n) {answer["N"] = n} 
+    if (p) {answer["P"] = p}     
+    
+    return answer;
+
 }
